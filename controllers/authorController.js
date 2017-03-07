6e8487388750027fb8ced6ec89a1594a4ad231db
Joe Boland <josephjboland@gmail.com>
2017-03-06 21:50:15 -0500

    Implement updates for bookinstance, author, genre

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -126,10 +126,62 @@ exports.author_delete_post = function(req, res, next) {
 
 // Display Author update form on GET
 exports.author_update_get = function(req, res, next) {
-    res.send('NOT IMPLEMENTED: Author update GET' );
+    req.sanitize('id').escape();
+    req.sanitize('id').trim();
+
+    async.parallel({
+            author: function(callback) {
+                Author.findById(req.params.id)
+                    .exec(callback);
+            },
+            books: function(callback) {
+                Book.find({'author': req.params.id}, 'title summary')
+                    .exec(callback);
+            }
+        },
+        function(err, results) {
+            if (err) { return next(err); }
+            res.render('author_form', { title: 'Update Author', author: results.author, books: results.books });
+        });
 };
 
 // Handle Author update on POST
 exports.author_update_post = function(req, res, next) {
-    res.send('NOT IMPLEMENTED: Author update POST' );
+    req.sanitize('id').escape();
+    req.sanitize('id').trim();
+
+    req.checkBody('first_name', 'First name required').notEmpty();
+    req.checkBody('family_name', 'Family name required').notEmpty();
+    req.checkBody('family_name', 'First name needs to be alphanumeric').isAlpha();
+
+    req.checkBody('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true}).isDate();
+    req.checkBody('date_of_death', 'Invalid date of death').optional({ checkFalsy: true}).isDate();
+
+    req.sanitize('first_name').escape();
+    req.sanitize('family_name').escape();
+    req.sanitize('first_name').trim();     
+    req.sanitize('family_name').trim();
+    req.sanitize('date_of_birth').toDate();
+    req.sanitize('date_of_death').toDate();
+
+    var errors = req.validationErrors();
+
+    var author = new Author({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+        _id: req.params.id
+    });
+
+    if (errors) {
+        res.render('author_form', { title: 'Update Author', errors: errors, author: author});
+        return;
+    } else {
+        Author.findByIdAndUpdate(req.params.id, author, function(err, theauthor) {
+            if (err) { return next(err); }
+            res.redirect(theauthor.url);
+        });
+    }
+
 };
--- controllers/genreController.js
+++ controllers/genreController.js
@@ -132,10 +132,60 @@ exports.genre_delete_post = function(req, res, next) {
 
 // Display Genre update form on GET
 exports.genre_update_get = function(req, res, next) {
-    res.send('NOT IMPLEMENTED: Genre update GET' );
+    req.sanitize('id').escape();
+    req.sanitize('id').trim();
+
+    Genre.findById(req.params.id)
+            .exec(function(err, genre) {
+                if (err) { return next(err); }
+                res.render('genre_form', { title: 'Update Genre', genre: genre });
+        });
 };
 
 // Handle Genre update on POST
 exports.genre_update_post = function(req, res, next) {
-    res.send('NOT IMPLEMENTED: Genre update POST' );
+    req.sanitize('id').escape();
+    req.sanitize('id').trim();
+
+    //Check that the name field is not empty
+    req.checkBody('name', 'Genre name required').notEmpty(); 
+    
+    //Trim and escape the name field. 
+    req.sanitize('name').escape();
+    req.sanitize('name').trim();
+    
+    //Run the validators
+    var errors = req.validationErrors();
+
+    //Create a genre object with escaped and trimmed data.
+    var genre = new Genre({ 
+            name: req.body.name,
+            _id: req.params.id
+        });
+    
+    if (errors) {
+        //If there are errors render the form again, passing the previously entered values and errors
+        res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors});
+        return;
+    } else {
+        // Data from form is valid.
+        //Check if Genre with same name already exists
+        Genre.findOne({ 'name': req.body.name })
+            .exec(function(err, found_genre) {
+                 console.log('found_genre: ' + found_genre);
+                 if (err) { return next(err); }
+                 
+                 if (found_genre) { 
+                     //Genre exists, redirect to its detail page
+                     res.redirect(found_genre.url);
+                 } else {  
+                    Genre.findByIdAndUpdate(req.params.id, genre, function(err, thegenre) {
+                        if (err) { return next(err); }
+                        //Genre saved. Redirect to genre detail page
+                        res.redirect(thegenre.url);
+                    });
+                }
+            });
+    }    
+
 };
